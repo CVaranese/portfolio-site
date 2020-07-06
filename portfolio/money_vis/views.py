84598b661ace367bec05a8c51738a5fb4c16a455
Chris <chris.varanese@stellar-exploration.com>
2020-07-06 12:29:05 -0700

    removed key

--- portfolio/money_vis/views.py
+++ portfolio/money_vis/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    api_key = 'Ksj9W0U9cIEmxqtCbpSXa77Yy4TATCFCnJPYB261'
     ctxt = {}
     
     return render(request, 'money_vis/index.html', ctxt)
